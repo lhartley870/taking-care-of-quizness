b330aac6be16df0618f0286ecf87d16b1ee01bd7
lhartley870 <hartleylc@hotmail.co.uk>
2021-07-29 13:25:48 +0000

    Add content of showAndHideRules function to show and hide quiz rules when 'Rules' button is clicked on home page

--- assets/js/home-page.js
+++ assets/js/home-page.js
@@ -17,8 +17,49 @@ document.addEventListener('DOMContentLoaded', function() {
   } 
 })
 
+/**
+ * The function called on the 'Rules' button when the script is first loaded
+ * allowing the user to click the 'Rules' button to display the quiz rules
+ * and click the button again to hide them
+ */
 function showAndHideRules() {
-  console.log('show and hide rules');
+  let rulesInnerContainer = document.getElementById('rules-container').children[1]; 
+  let rules = `
+    <ol>
+      <li>Click on either 'Start Quiz 1' or 'Start Quiz 2' to begin</li>
+      <li>There are 5 rounds per quiz and 3 questions in a round</li>
+      <li>All questions are multiple choice with 1 correct answer per question</li>
+      <li>Click the answers you want to submit for each round and then click the 'Submit Round' button to see how you did in that round</li>
+      <li>When you have submitted your Round 5 questions, click on the 'Get My Results' button to find out your overall quiz score and stats</li>
+      <li>
+        <table>
+          <tr>
+            <th>Score</th>
+            <th>You are</th>
+          </tr>
+          <tr class="low-score">
+            <td>0-5 <i class="far fa-frown"></i></td>
+            <td>Les Quizerables</td>
+          </tr>
+          <tr class="medium-score">
+            <td>6-10 <i class="far fa-meh"></i></td>
+            <td>Risky Quizness</td>
+          </tr>
+          <tr class="high-score">
+            <td>11-15 <i class="far fa-smile"></i></td>
+            <td>The Quizard of Oz</td>
+          </tr>
+        </table>
+      </li>
+      <li>Once you have completed one quiz, come back and try the other one!</li>
+    </ol>
+  `; 
+
+  if (rulesInnerContainer.innerHTML === " ") {
+    rulesInnerContainer.innerHTML = rules; 
+  } else {
+    rulesInnerContainer.innerHTML = " "; 
+  }
 }
 
 function showAndHideUsernameForm() {
