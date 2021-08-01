a13d6176b9f8528d0bfa157466fc59bc8cf61c0e
lhartley870 <hartleylc@hotmail.co.uk>
2021-08-01 21:12:42 +0000

    Add click event listener on answer 'button' divs and radioSelect function to click associated radio button when div is clicked

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,6 +1,23 @@
 // Wait for the DOM to finish loading before adding quiz interactivity
 document.addEventListener('DOMContentLoaded', function() {
-          
+  let answerButtons = document.getElementsByClassName('quiz-answer'); 
+
+  for (let answerButton of answerButtons) {
+      answerButton.addEventListener('click', radioSelect); 
   }
 })
+
+/**
+ * The function called when the user clicks an answer 'button' 
+ * (div containing input and label elements) so that clicking 
+ * anywhere in the div selects the radio button rather than the 
+ * user having to specifically click the radio button or the label 
+ * text to select the quiz answer
+ */
+ function radioSelect(event) {
+  let radioButton = this.children[0]; 
+
+  radioButton.click(); 
+}
+
 
