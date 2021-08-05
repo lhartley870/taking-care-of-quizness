cb2b1d78c967867ba964734076f7adadbcc1c216
lhartley870 <hartleylc@hotmail.co.uk>
2021-08-05 15:58:02 +0000

    Add function to run on document loading calling sub-functions, add structure of sub-functions

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,3 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+  displayUsername(); 
+  calculateTotalScore(); 
+  displayRoundStats(); 
+})
+
+function displayUsername() {
+  
+}
+
+function calculateTotalScore() {
+  
+}
+
+function displayRoundStats() {
+
+}
+
+function findBestRoundsScores() {
+
+}
+
+function findBestRounds() {
+
+}
 
 
 
